41cdbd8e0594cd184db82b7965fe77c87b87b901
venci0003 <venci0003@gmail.com>
2022-10-23 19:59:54 +0300

    Uploaded solutions to - Text Processing - Lab

---
+++ Text Processing - Lab/02. Repeat Strings/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02._Repeat_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Text Processing - Lab/04. Text Filter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _04._Text_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
