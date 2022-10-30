64f1c6446222038c60d23523b6b5927bd89761ce
venci0003 <venci0003@gmail.com>
2022-10-30 19:01:44 +0200

    Uploaded solutions to - Regular Expressions - Lab

---
+++ Regular Expressions - Lab/01. Match Full Name/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01._Match_Full_Name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Regular Expressions - Lab/02. Match Phone Number/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02._Match_Phone_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
